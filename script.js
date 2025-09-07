dd7e0f8fe5020bf52495adfc37db5050dc54e9bf
Kevin De luca <kevindeluca2006@gmail.com>
2025-09-07 19:36:44 +0200

    base pagina getintouch

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/*
- document.querySelectorAll(".animazione-fadeIn").forEach(element => {
-   element.style.opacity = 1;
-});
-*/
-
 function changeLanguage(countryCode, languageName) {
     const button = document.getElementById('languageButton');
     button.className = `language-button ${countryCode}`;
